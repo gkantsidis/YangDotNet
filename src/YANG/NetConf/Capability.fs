081c9e065991027f138db1d14b948ce421f2e3fe
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2019-05-13 16:57:41 +0100

    feat(NetConf): Moved capability code in separate module

---
+++ src/YANG/NetConf/Capability.fs
@@ -0,0 +1,117 @@
+﻿namespace Yang.NetConf
+
+module Capability =
+    open System
+    open Yang.Model
+
+    let ReadCapabilities (document : Xml.XmlDocument) =
+        let raw = ((document.Item "hello").Item "capabilities").ChildNodes
+        List.init raw.Count (fun i -> raw.Item i)
+        |> List.map (
+            fun capability ->
+                assert (capability.LocalName.Equals("capability", StringComparison.InvariantCultureIgnoreCase))
+                assert (capability.ChildNodes.Count = 1)
+                assert (capability.FirstChild.LocalName.Equals("#text", StringComparison.InvariantCultureIgnoreCase))
+                capability.FirstChild.Value
+        )
+
+    let TryGetVersion (uri : Uri) =
+        if uri.Scheme.Equals("http", StringComparison.InvariantCultureIgnoreCase) then
+            if uri.Segments.Length > 0 then
+                match Version.TryParse(uri.Segments.[uri.Segments.Length - 1]) with
+                | true, version     -> Some version
+                | false, _          -> None
+            else None
+        elif uri.Scheme.Equals("urn", StringComparison.InvariantCultureIgnoreCase) then
+            let index = uri.AbsolutePath.LastIndexOf(':')
+            if index < 1 then None
+            else
+                let suffix = uri.AbsolutePath.Substring(index + 1)
+                match Version.TryParse(suffix) with
+                | true, version     -> Some version
+                | false, _          -> None
+        else
+            None
+
+    let GetOptions (uri : Uri) =
+        if uri.Query.Length = 0 then Map.empty
+        else
+            assert (uri.Query.StartsWith("?"))
+            uri.Query.Substring(1).Split('&')
+            |> Seq.map(
+                fun option ->
+                    let index = option.IndexOf('=')
+                    assert(index > 0)
+                    let name = option.Substring(0, index)
+                    let value = option.Substring(index+1)
+                    name, value
+            )
+            |> Map.ofSeq
+
+
+    let netconf_capability_prefix = "urn:ietf:params:netconf:capability:"
+
+    type Capability = {
+        Name        : Uri
+        Prefix      : Uri
+        Options     : Map<string, string>
+        Revision    : Arguments.Date option
+        Version     : Version option
+    }
+    with
+        static member Make (name : string) =
+            assert (System.String.IsNullOrWhiteSpace(name) = false)
+            let uri = Uri(name)
+            let baseUri =
+                uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.Length - uri.Query.Length) |> Uri
+            let options = GetOptions uri
+
+            let revision =
+                    match options.TryFind "revision" with
+                    | None      -> None
+                    | Some rev  -> Arguments.Date.Make rev |> Some
+
+            {
+                Name        = uri
+                Prefix      = baseUri
+                Options     = options
+                Revision    = revision
+                Version     = TryGetVersion uri
+            }
+
+        member this.NetConfCapabilityName =
+            if this.Prefix.AbsoluteUri.StartsWith(netconf_capability_prefix, StringComparison.InvariantCultureIgnoreCase) then
+                let ending = this.Prefix.AbsoluteUri.Substring(netconf_capability_prefix.Length)
+                let index = ending.IndexOf(':')
+                assert(index > 0)
+                let name = ending.Substring(0, index)
+                Some name
+            else None
+
+
+    type StandardCapabilitity =
+    | WritableRunning
+    | CandidateConfiguration
+    | ConfirmedCommit
+    | RollbackOnError
+    | Validate
+    | DistinctStartup
+    | Url of string list
+    | XPath
+    with
+        static member TryMake (capability : Capability) =
+            match capability.NetConfCapabilityName with
+            | None      -> None
+            | Some name ->
+                if name.Equals("writable-running", StringComparison.InvariantCultureIgnoreCase)     then Some WritableRunning
+                elif name.Equals("candidate", StringComparison.InvariantCultureIgnoreCase)          then Some CandidateConfiguration
+                elif name.Equals("confirmed-commit", StringComparison.InvariantCultureIgnoreCase)   then Some ConfirmedCommit
+                elif name.Equals("rollback-on-error", StringComparison.InvariantCultureIgnoreCase)  then Some RollbackOnError
+                elif name.Equals("validate", StringComparison.InvariantCultureIgnoreCase)           then Some Validate
+                elif name.Equals("startup", StringComparison.InvariantCultureIgnoreCase)            then Some DistinctStartup
+                elif name.Equals("url", StringComparison.InvariantCultureIgnoreCase)                then
+                    assert(capability.Options.ContainsKey("scheme"))
+                    let options = capability.Options.["scheme"]
+                    Some (Url (options.Split(',') |> Array.toList))
+                elif name.Equals("xpath", StringComparison.InvariantCultureIgnoreCase)              then Some XPath
+                else None
--- src/YANG/NetConf/playground.fsx
+++ src/YANG/NetConf/playground.fsx
@@ -7,10 +7,11 @@
 #r @"../Model/bin/Debug/Yang.Model.dll"
 
 #load "Client.fs"
+#load "Capability.fs"
 
 open System
 open Renci.SshNet
-open Yang.Model
+open Yang.NetConf
 
 // Define your library scripting code here
 
@@ -26,135 +27,50 @@ let client = new NetConfClient(machine, port, user, password)
 
 let ConnectAsync (client : NetConfClient) = async { client.Connect() }
 
-client.ClientCapabilities
+Async.RunSynchronously(ConnectAsync client)
+assert(client.IsConnected)
 
-let xx = client.ServerCapabilities
-let session_id = ((xx.Item "hello").Item "session-id").InnerText |> Int32.Parse
-let capabilities =
-    let raw = ((xx.Item "hello").Item "capabilities").ChildNodes
-    List.init raw.Count (fun i -> raw.Item i)
-    |> List.map (
-        fun capability ->
-            assert (capability.LocalName.Equals("capability", StringComparison.InvariantCultureIgnoreCase))
-            assert (capability.ChildNodes.Count = 1)
-            assert (capability.FirstChild.LocalName.Equals("#text", StringComparison.InvariantCultureIgnoreCase))
-            capability.FirstChild.Value
-    )
+let example = """
+<rpc message-id="205" xmlns="urn:ietf:params:xml:ns:netconf:base:1.1">
+    <get-config>
+        <source>
+            <running/>
+        </source>
+    </get-config>
+</rpc>
+"""
 
+let request = new Xml.XmlDocument()
+let item = request.AppendChild(request.CreateElement("get-config"))
+let source = item.AppendChild(request.CreateElement("source"))
+let running = source.AppendChild(request.CreateElement("running"))
+request.OuterXml
+
+client.IsConnected
+client.AutomaticMessageIdHandling <- false
+let r2 = client.SendReceiveRpc(request)
+let response = client.SendReceiveRpc(example)
+
+let GetSessionId (document : Xml.XmlDocument) =
+    ((document.Item "hello").Item "session-id").InnerText |> Int32.Parse
+
+let session_id = GetSessionId client.ServerCapabilities
+printfn "Session id: %d" session_id
+
+let capabilities = Capability.ReadCapabilities client.ServerCapabilities
+let client_capabilities = Capability.ReadCapabilities client.ClientCapabilities
 capabilities |> List.iter (printfn "%A")
 
-let tryGetVersion (uri : Uri) =
-    if uri.Scheme.Equals("http", StringComparison.InvariantCultureIgnoreCase) then
-        if uri.Segments.Length > 0 then
-            match Version.TryParse(uri.Segments.[uri.Segments.Length - 1]) with
-            | true, version     -> Some version
-            | false, _          -> None
-        else None
-    elif uri.Scheme.Equals("urn", StringComparison.InvariantCultureIgnoreCase) then
-        let index = uri.AbsolutePath.LastIndexOf(':')
-        if index < 1 then None
-        else
-            let suffix = uri.AbsolutePath.Substring(index + 1)
-            match Version.TryParse(suffix) with
-            | true, version     -> Some version
-            | false, _          -> None
-    else
-        None
-
 let uri = Uri("urn:ietf:params:xml:ns:netconf:base:1.0?module=ietf-netconf&revision=2011-06-01")
-let version = tryGetVersion (uri)
+let version = Capability.TryGetVersion (uri)
 assert(version.IsSome)
 assert(Version(1, 0) = version.Value)
 
-
-let getOptions (uri : Uri) =
-    if uri.Query.Length = 0 then Map.empty
-    else
-        assert (uri.Query.StartsWith("?"))
-        uri.Query.Substring(1).Split('&')
-        |> Seq.map(
-            fun option ->
-                let index = option.IndexOf('=')
-                assert(index > 0)
-                let name = option.Substring(0, index)
-                let value = option.Substring(index+1)
-                name, value
-        )
-        |> Map.ofSeq
-
-
-let netconf_capability_prefix = "urn:ietf:params:netconf:capability:"
-
-type Capability = {
-    Name        : Uri
-    Prefix      : Uri
-    Options     : Map<string, string>
-    Revision    : Arguments.Date option
-    Version     : Version option
-}
-with
-    static member Make (name : string) =
-        assert (System.String.IsNullOrWhiteSpace(name) = false)
-        let uri = Uri(name)
-        let baseUri =
-            uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.Length - uri.Query.Length) |> Uri
-        let options = getOptions uri
-
-        let revision =
-                match options.TryFind "revision" with
-                | None      -> None
-                | Some rev  -> Arguments.Date.Make rev |> Some
-
-        {
-            Name        = uri
-            Prefix      = baseUri
-            Options     = options
-            Revision    = revision
-            Version     = tryGetVersion uri
-        }
-
-    member this.NetConfCapabilityName =
-        if this.Prefix.AbsoluteUri.StartsWith(netconf_capability_prefix, StringComparison.InvariantCultureIgnoreCase) then
-            let ending = this.Prefix.AbsoluteUri.Substring(netconf_capability_prefix.Length)
-            let index = ending.IndexOf(':')
-            assert(index > 0)
-            let name = ending.Substring(0, index)
-            Some name
-        else None
-
-let cc = capabilities |> List.map Capability.Make
+let cc = capabilities |> List.map Capability.Capability.Make
 cc |> List.map (fun cap -> cap.Name.OriginalString, cap.Version) |> List.iter (printfn "%A")
 cc |> List.map (fun cap -> cap.Name.OriginalString, cap.Revision) |> List.iter (printfn "%A")
 cc |> List.map (fun cap -> cap.Name.OriginalString, cap.NetConfCapabilityName) |> List.iter (printfn "%A")
 
-
-type StandardCapabilitity =
-| WritableRunning
-| CandidateConfiguration
-| ConfirmedCommit
-| RollbackOnError
-| Validate
-| DistinctStartup
-| Url of string list
-| XPath
-with
-    static member TryMake (capability : Capability) =
-        match capability.NetConfCapabilityName with
-        | None      -> None
-        | Some name ->
-            if name.Equals("writable-running", StringComparison.InvariantCultureIgnoreCase)     then Some WritableRunning
-            elif name.Equals("candidate", StringComparison.InvariantCultureIgnoreCase)          then Some CandidateConfiguration
-            elif name.Equals("confirmed-commit", StringComparison.InvariantCultureIgnoreCase)   then Some ConfirmedCommit
-            elif name.Equals("rollback-on-error", StringComparison.InvariantCultureIgnoreCase)  then Some RollbackOnError
-            elif name.Equals("validate", StringComparison.InvariantCultureIgnoreCase)           then Some Validate
-            elif name.Equals("startup", StringComparison.InvariantCultureIgnoreCase)            then Some DistinctStartup
-            elif name.Equals("url", StringComparison.InvariantCultureIgnoreCase)                then
-                assert(capability.Options.ContainsKey("scheme"))
-                let options = capability.Options.["scheme"]
-                Some (Url (options.Split(',') |> Array.toList))
-            elif name.Equals("xpath", StringComparison.InvariantCultureIgnoreCase)              then Some XPath
-            else None
-
-cc |> List.choose (StandardCapabilitity.TryMake)
+cc |> List.choose (Capability.StandardCapabilitity.TryMake)
 
 client.Dispose()
